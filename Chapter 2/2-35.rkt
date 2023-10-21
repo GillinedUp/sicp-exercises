e3349abd4416453126738ff827a9cf21fa36c667
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2023-10-21 22:10:39 +0200

    complete 2-35

--- Chapter 2/2-35.rkt
+++ Chapter 2/2-35.rkt
@@ -1,7 +1,5 @@
 #lang sicp
 
-; WIP
-
 (define (accumulate op initial sequence)
   (if (null? sequence)
       initial
@@ -9,14 +7,14 @@
           (accumulate op initial (cdr sequence)))))
 
 (define (count-leaves t)
-  (accumulate (lambda (x y) (+ 1 y))
+  (accumulate (lambda (x y) (+ x y))
               0
               (map (lambda (x)
                      (if (not (pair? x))
-                         x
+                         1
                          (count-leaves x)))
                    t)))
 
-(define y (list (list 1 2 (list 3 4)) 5 6))
+(define y (list (list 1 2 (list (list 3 4) 5)) 6 (list 7 8) 9))
 
 (count-leaves y)
