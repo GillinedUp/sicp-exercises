cb75a90ea125f93d14e9f22acf22748af72a4553
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2024-09-06 22:44:33 +0200

    update 1-22.rkt

--- Chapter 1/1-22.rkt
+++ Chapter 1/1-22.rkt
@@ -1,36 +1,65 @@
 #lang sicp
 
-(define (square a) (* a a))
+(define (square x)
+  (* x x))
 
-(define (smallest-divisor n)
-  (find-divisor n 2))
+(define (divides? a b)
+  (= (remainder b a) 0))
 
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
         (else (find-divisor n (+ test-divisor 1)))))
 
-(define (divides? a b)
-  (= (remainder b a) 0))
+(define (smallest-divisor n)
+  (find-divisor n 2))
 
 (define (prime? n)
   (= n (smallest-divisor n)))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
 
 (define (timed-prime-test n)
   (newline)
   (display n)
   (start-prime-test n (runtime)))
 
-(define (start-prime-test n start-time)
-  (if (prime? n)
-      (report-prime (- (runtime) start-time))))
+(define (search-for-primes start end)
+  (timed-prime-test start)
+  (if (< start end)
+      (search-for-primes
+       (+ start (if (even? start) 1 2))
+       end)))
 
-(define (report-prime elapsed-time)
-  (display " *** ")
-  (display elapsed-time))
+#|
 
-(define (search-for-primes a b)
-  (timed-prime-test a)
-  (if (< a b) (search-for-primes (+ a 1) b)))
+1009 *** 3
+1013 *** 2
+1019 *** 2
+10007 *** 13
+10009 *** 11
+10037 *** 13
+100003 *** 15
+100019 *** 17
+100043 *** 16
+1000003 *** 34
+1000033 *** 37
+1000037 *** 37
 
-(search-for-primes 10000 15000)
+(sqrt 1000) -> 31.62
+(sqrt 10000) -> 100
+(sqrt 100000) -> 316.22
+(sqrt 1000000) -> 1000
+
+Measured runtime of this algorithm againts different inputs doesn't really compare
+with it's order of growth (O(sqrt(n)), specifically between 10000 and 100000 or
+100000 and 1000000. Looks like the algorithm runs faster that the order of
+growth prediction.
+
+|#
