b69c549e225600a1b3ce74c8981ea09057b088f6
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2024-09-22 22:58:01 +0200

    update 1-46

--- Chapter 1/1-46.rkt
+++ Chapter 1/1-46.rkt
@@ -1,30 +1,29 @@
 #lang sicp
 
 (define (iterative-improve good-enough? improve)
-  (define (iter guess)
-    (if (good-enough? guess)
-        guess
-        (iter (improve guess))))
-  (lambda (guess) (iter guess)))
- 
+  (lambda (guess)
+    (define (iter current-guess)
+      (if (good-enough? current-guess)
+          current-guess
+          (iter (improve current-guess))))
+    (iter guess)))
+
 (define (sqrt x)
-  (define guess 2.0)
-  (define (average a b)
-    (/ (+ a b) 2))
   ((iterative-improve
     (lambda (guess)
-      (< (abs (- (expt guess 2) x)) 0.001))
+      (let ((square (lambda (a) (* a a)))
+            (tolerance 0.001))
+        (< (abs (- (square guess) x)) tolerance)))
     (lambda (guess)
-      (average guess (/ x guess))))
-   guess))
+      (let ((average (lambda (a b) (/ (+ a b) 2))))
+        (average guess (/ x guess)))))
+   1.0))
 
 (define (fixed-point f first-guess)
   ((iterative-improve
     (lambda (guess)
-        (< (abs (- guess (f guess))) 0.001))
+      (let ((tolerance 0.00001))
+        (< (abs (- guess (f guess))) tolerance)))
     (lambda (guess)
-        (f guess)))
+      (f guess)))
    first-guess))
-
-(fixed-point cos 1.0)
-(sqrt 16.0)
