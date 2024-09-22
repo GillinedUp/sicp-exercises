8e0b497fade8b07b62a8cead00e2117769b02f01
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2024-09-22 22:22:58 +0200

    update 1-45

--- Chapter 1/1-45.rkt
+++ Chapter 1/1-45.rkt
@@ -1,39 +1,38 @@
 #lang sicp
 
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
+
+(define (repeated f n)
+  (define (iter acc k)
+    (if (= k 1)
+        acc
+        (iter (compose f acc) (- k 1))))
+  (iter f n))
+
 (define (average a b)
   (/ (+ a b) 2))
 
 (define (average-damp f)
   (lambda (x) (average x (f x))))
 
-(define tolerance 0.00001)
-
 (define (fixed-point f first-guess)
   (define (close-enough? v1 v2)
-    (< (abs (- v1 v2)) tolerance))
+    (let ((tolerance 0.0001))
+      (< (abs (- v1 v2)) tolerance)))
   (define (try guess)
     (let ((next (f guess)))
-      (display next)
-      (newline)
       (if (close-enough? guess next)
           next
           (try next))))
   (try first-guess))
 
-(define (compose f g)
-  (lambda (x)
-    (f (g x))))
-
-(define (repeated f n)
-  (if (= n 1)
-      (lambda (x) (f x))
-      (repeated (compose f f) (- n 1))))
-
 (define (n-root x n)
-  (let ((guess 2.0)
-        (damp-times 3))
-    (fixed-point
-     ((repeated average-damp damp-times) (lambda (y) (/ x (expt y (- n 1)))))
-     guess)))
+  (let ((damp-num (floor (log n 2))))
+    (fixed-point ((repeated average-damp damp-num)
+                  (lambda (y)
+                    (/ x (expt y (- n 1)))))
+                 1.0)))
 
-(n-root 1.0 2)
+; To compute n-th root of a number, you need to take (floor (log n 2)) average-damps.
