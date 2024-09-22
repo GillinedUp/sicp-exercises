8cba8e89637be518357a2778492b00768825470e
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2024-09-22 21:36:33 +0200

    update 1-44

--- Chapter 1/1-44.rkt
+++ Chapter 1/1-44.rkt
@@ -1,20 +1,23 @@
 #lang sicp
 
-(define (smooth f)
-  (let ((dx 0.0001))
-    (lambda (x)
-      (/ (+ (f x) (f (- x dx)) (f (+ x dx))) 3))))
-
 (define (compose f g)
   (lambda (x)
     (f (g x))))
 
 (define (repeated f n)
-  (if (= n 1)
-      (lambda (x) (f x))
-      (repeated (compose f f) (- n 1))))
+  (define (iter acc k)
+    (if (= k 1)
+        acc
+        (iter (compose f acc) (- k 1))))
+  (iter f n))
 
-(define (square x) (* x x))
+(define (smooth f)
+  (let ((dx 0.00001))
+    (lambda (x)
+      (/ (+ (f (- x dx))
+            (f x)
+            (f (+ x dx)))
+         3))))
 
-((repeated (smooth square) 2.0) 5)  
-
+(define (n-fold-smooth f n)
+  ((repeated smooth n) f))
