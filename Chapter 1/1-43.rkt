41a19303ab04f825d4c49987273a421d486db184
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2024-09-22 19:31:48 +0200

    update 1-43

--- Chapter 1/1-43.rkt
+++ Chapter 1/1-43.rkt
@@ -4,11 +4,14 @@
   (lambda (x)
     (f (g x))))
 
+; (define (repeated f n)
+;   (if (= n 1)
+;       f
+;       (compose f (repeated f (- n 1)))))
+
 (define (repeated f n)
-  (if (= n 1)
-      (lambda (x) (f x))
-      (repeated (compose f f) (- n 1))))
-
-(define (square x) (* x x))
-
-((repeated square 2) 6)    
+  (define (iter acc k)
+    (if (= k 1)
+        acc
+        (iter (compose f acc) (- k 1))))
+  (iter f n))
