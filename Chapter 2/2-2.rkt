fcad635264b9fd6cc238ba23a59a7d2228caf640
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2024-09-28 16:02:05 +0200

    update 2-2

--- Chapter 2/2-2.rkt
+++ Chapter 2/2-2.rkt
@@ -1,6 +1,15 @@
 #lang sicp
 
-; abstraction barrier - segment package, depends on "point", "pair" and "average" packages
+; abstraction barrier: middpoint-segment package, depends on segment and point package
+
+(define (midpoint-segment s)
+  (make-point
+   (average (x-point (start-segment s))
+            (x-point (end-segment s)))
+   (average (y-point (start-segment s))
+            (y-point (end-segment s)))))
+
+; abstraction barrier: segment package, depends on pair and average packages
 
 (define (make-segment start end)
   (cons start end))
@@ -11,14 +20,17 @@
 (define (end-segment s)
   (cdr s))
 
-(define (midpoint-segment s)
-  (make-point
-   (average (x-point (start-segment s))
-            (x-point (end-segment s)))
-   (average (y-point (start-segment s))
-            (y-point (end-segment s)))))
+; abstraction barrier: print-point package, depends on point package
 
-; abstraction barrier - "point" package, depends on "pair" package
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+; abstraction barrier: point package, depends on pair package
 
 (define (make-point x y)
   (cons x y))
@@ -29,15 +41,7 @@
 (define (y-point p)
   (cdr p))
 
-(define (print-point p)
-  (newline)
-  (display "(")
-  (display (x-point p))
-  (display ",")
-  (display (y-point p))
-  (display ")"))
-
-; abstraction barrier - "average" package
+; abstraction barrier: average package
 
 (define (average a b)
   (/ (+ a b) 2.0))
