c85f8d455724fa46a08f171f6b3a6fb931820644
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2024-10-12 21:49:31 +0200

    update 2-18

--- Chapter 2/2-18.rkt
+++ Chapter 2/2-18.rkt
@@ -1,19 +1,9 @@
 #lang sicp
 
 (define (reverse items)
-  (define (iter list-in list-out)
-    (if (null? list-in)
-        list-out
-        (iter
-         (cdr list-in)
-         (cons (car list-in) list-out))))
-  (iter items nil))
-
-(define (reverse-rec items)
-  (if (null? items)
-      nil
-      (append
-       (reverse-rec (cdr items))
-       (list (car items)))))
-
-(reverse-rec (list 1 4 9 16 25))
+  (define (move from-list to-list)
+    (if (null? from-list)
+        to-list
+        (move (cdr from-list)
+              (cons (car from-list) to-list))))
+  (move items nil))
