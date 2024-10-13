2919c7571ef23d757e3a00cb3fcfd4a63dbe8589
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2024-10-13 12:29:52 +0200

    update 2-23

--- Chapter 2/2-23.rkt
+++ Chapter 2/2-23.rkt
@@ -1,13 +1,8 @@
 #lang sicp
 
 (define (for-each proc items)
-  (define (iter proc items side-effect)
-    (if (null? items)
-        true
-        (iter proc (cdr items) (proc (car items)))))
-  (iter proc items 1))
-
-(for-each
- (lambda (x) (newline) (display x))
- '(57 321 88))
+  (if (null? items)
+      true
+      (let ((_ (proc (car items))))
+        (for-each proc (cdr items)))))
           
