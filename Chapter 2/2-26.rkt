340c20343e19818ee0c633c67e76523f6f216bc0
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2025-03-30 21:35:09 +0200

    update 2-26

--- Chapter 2/2-26.rkt
+++ Chapter 2/2-26.rkt
@@ -4,8 +4,15 @@
 
 (define y (list 4 5 6))
 
-(append x y)
+(append x y) ; (1 2 3 4 5 6)
 
-(cons x y)
+(cons x y) ; ((1 2 3) 4 5 6)
 
-(list x y)
+(list x y) ; ((1 2 3) (4 5 6))
+
+; Explanation:
+; (list x y) 
+; (cons x (cons y nil)) 
+; (cons '(1 2 3) (cons '(4 5 6) nil))
+; (cons '(1 2 3) '((4 5 6)))
+; ((1 2 3) (4 5 6))
