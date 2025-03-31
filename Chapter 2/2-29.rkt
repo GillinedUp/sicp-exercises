784f8ae381b25e6a438a8a8fe6e15825cdd4333a
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2025-03-31 22:00:33 +0200

    update 2-29

--- Chapter 2/2-29.rkt
+++ Chapter 2/2-29.rkt
@@ -1,13 +1,29 @@
 #lang sicp
 
+; list representation
+
 ; (define (make-mobile left right)
 ;   (list left right))
 
-(define (make-mobile left right)
-  (cons left right))
-
 ; (define (make-branch length structure)
 ;   (list length structure))
+
+; (define (left-branch mobile)
+;   (car mobile))
+
+; (define (right-branch mobile)
+;   (car (cdr mobile)))
+
+; (define (branch-length branch)
+;   (car branch))
+
+; (define (branch-structure branch)
+;   (car (cdr branch)))
+
+; cons representation
+
+(define (make-mobile left right)
+  (cons left right))
 
 (define (make-branch length structure)
   (cons length structure))
@@ -15,56 +31,60 @@
 (define (left-branch mobile)
   (car mobile))
 
-; (define (right-branch mobile)
-;   (car (cdr mobile)))
-
 (define (right-branch mobile)
   (cdr mobile))
 
 (define (branch-length branch)
   (car branch))
 
-; (define (branch-structure branch)
-;   (car (cdr branch)))
-
 (define (branch-structure branch)
   (cdr branch))
 
-(define (total-weight mobile)
-  (define (weight-structure structure)
-    (if (pair? structure)
-        (total-weight structure)
-        structure))
-  (if (not (pair? mobile)) 
-      mobile
-      (let ((left-structure (branch-structure (left-branch mobile)))
-            (right-structure (branch-structure (right-branch mobile))))
-        (+ (weight-structure left-structure)
-           (weight-structure right-structure)))))
-  
-(define (balanced? mobile)
-  (if (not (pair? mobile))
-      #t
-      (let ((left (left-branch mobile))
-            (right (right-branch mobile)))
-        (let ((left-structure (branch-structure left))
-              (right-structure (branch-structure right)))
-          (and (balanced? left-structure)
-               (balanced? right-structure)
-               (= (* (branch-length left) 
-                     (total-weight left-structure))
-                  (* (branch-length right) 
-                     (total-weight right-structure))))))))
+; abstraction barrier - operations on mobiles
 
-(define x 
+(define (total-weight mobile)
+  (if (not (pair? mobile))
+      mobile
+      (let ((left-branch-structure (branch-structure (left-branch mobile)))
+            (right-branch-structure (branch-structure (right-branch mobile))))
+        (+ (total-weight left-branch-structure)
+           (total-weight right-branch-structure)))))
+
+(define (balanced? mobile)
+  (define (branch-torque branch)
+    (* (branch-length branch)
+       (total-weight (branch-structure branch))))
+  (define (balanced-mobile? mobile)
+    (let ((left-branch-torque (branch-torque (left-branch mobile)))
+          (right-branch-torque (branch-torque (right-branch mobile)))
+          (left-branch-structure (branch-structure (left-branch mobile)))
+          (right-branch-structure (branch-structure (right-branch mobile))))
+      (and (= left-branch-torque right-branch-torque)
+           (balanced? left-branch-structure)
+           (balanced? right-branch-structure))))
+  (if (not (pair? mobile))
+      true
+      (balanced-mobile? mobile)))
+
+; test
+
+(define x
   (make-mobile
-   (make-branch 2
-                (make-mobile
-                 (make-branch 3 4)
-                 (make-branch 4 3)))
-   (make-branch 2 
-                (make-mobile
-                 (make-branch 4 3) 
-                 (make-branch 3 4)))))
+   (make-branch
+    2
+    (make-mobile
+     (make-branch 3 4)
+     (make-branch 4 3)))
+   (make-branch
+    2
+    (make-mobile
+     (make-branch 4 3)
+     (make-branch 3 4)))))
+
+; should return 14
+
+(total-weight x)
+
+; should return true
 
 (balanced? x)
