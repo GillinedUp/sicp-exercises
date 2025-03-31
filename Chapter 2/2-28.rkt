a4f260840caf496e9c8235461a3dd83291f1cefb
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2025-03-31 20:38:37 +0200

    update 2-28

--- Chapter 2/2-28.rkt
+++ Chapter 2/2-28.rkt
@@ -1,15 +1,8 @@
 #lang sicp
 
-(define (fringe items)
-  (cond ((null? items) nil) 
-        ((not (pair? items)) items)
-        (else 
-         ((if (pair? (car items)) 
-              append 
-              cons)
-          (fringe (car items))
-          (fringe (cdr items))))))
-
-(define y (list (list 1 2 (list 3 4)) 5 6))
-
-(fringe y)
+(define (fringe tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else
+         (append (fringe (car tree))
+                 (fringe (cdr tree))))))
