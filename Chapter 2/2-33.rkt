e045e6990d2dc6ab108a74d7e8e31abc69a59fe6
Yurii Vyzhha <23532374+GillinedUp@users.noreply.github.com>
2025-04-05 20:02:03 +0200

    2-33

--- Chapter 2/2-33.rkt
+++ Chapter 2/2-33.rkt
@@ -6,23 +6,25 @@
       (op (car sequence)
           (accumulate op initial (cdr sequence)))))
 
+; abstraction barrier: sequence operations
+
 (define (map p sequence)
   (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
-
-; test, should return (1 4 9 16)
-
-(map (lambda (x) (* x x)) (list 1 2 3 4))
 
 (define (append seq1 seq2)
   (accumulate cons seq2 seq1))
 
-; test, should return (1 2 3 4 5 6)
-
-(append (list 1 2 3) (list 4 5 6))
-
 (define (length sequence)
   (accumulate (lambda (_ y) (+ 1 y)) 0 sequence))
 
-; test, should return 5
+; map test, should return (1 4 9 16)
+
+(map (lambda (x) (* x x)) (list 1 2 3 4))
+
+; append test, should return (1 2 3 4 5 6)
+
+(append (list 1 2 3) (list 4 5 6))
+
+; length test, should return 5
 
 (length (list 1 2 3 4 5))
